f356bc90f67c72528c34f6f889cac906e54f83e9
varshaaCodes <91754632+varshaaCodes@users.noreply.github.com>
2022-01-04 22:55:28 +0530

    Question 10

---
+++ 10_Max10Numbers.cpp
@@ -0,0 +1,45 @@
+//Company Name : Goldman Sachs
+
+//Question 10
+
+// Find max 10 numbers in a list having 10M entries.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+private:
+public:
+    vector<int> Max10Numbers(vector<int> &numbers)
+    {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        int n = numbers.size();
+        for (int i = 0; i < n; i++)
+        {
+            pq.push(numbers[i]);
+            if (pq.size() > 10)
+                pq.pop();
+        }
+        vector<int> res(10);
+        int i = 9;
+        while (!pq.empty())
+        {
+            res[i] = pq.top();
+            i--;
+            pq.pop();
+        }
+        return res;
+    }
+};
+
+
+int main()
+{
+    vector<int> num = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    Solution s;
+    vector<int> res = s.Max10Numbers(num);
+    for (auto &i : res)
+        cout << i << " ";
+}
