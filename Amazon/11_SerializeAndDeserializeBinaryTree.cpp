96f0698a9f92d6db1456dd963c4be6133ecb8337
varshaaCodes <91754632+varshaaCodes@users.noreply.github.com>
2022-01-10 23:16:17 +0530

    question 11

---
+++ Amazon/11_SerializeAndDeserializeBinaryTree.cpp
@@ -0,0 +1,61 @@
+// Serialize and Deserialize a Binary Tree
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+class Solution
+{
+    void solve(Node *root, vector<int> &res)
+    {
+        if (!root)
+        {
+            res.push_back(-1);
+            return;
+        }
+        res.push_back(root->data);
+        solve(root->left, res);
+        solve(root->right, res);
+    }
+
+    Node *build_tree(vector<int> &A, int &idx)
+    {
+        if ((A.size() - 1) < idx || A[idx] == -1)
+        {
+            idx++;
+            return NULL;
+        }
+        Node *root = new Node(A[idx++]);
+        root->left = build_tree(A, idx);
+        root->right = build_tree(A, idx);
+        return root;
+    }
+
+public:
+    // Function to serialize a tree and return a list containing nodes of tree.
+    vector<int> serialize(Node *root)
+    {
+        vector<int> ans;
+        solve(root, ans);
+        return ans;
+    }
+
+    Node *deSerialize(vector<int> &A)
+    {
+        int idx = 0;
+        return build_tree(A, idx);
+    }
+};
